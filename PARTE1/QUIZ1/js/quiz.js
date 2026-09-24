b04156f73ce5e7a5645bb714c07c5abb90794f41
lucas laos <laossim@outlook.com>
2026-09-24 14:36:22 -0300

    Update quiz.js

--- PARTE1/QUIZ1/js/quiz.js
+++ PARTE1/QUIZ1/js/quiz.js
@@ -4,3 +4,14 @@ function somarPonto() {
 }
 
 console.log("quiz.js carregado!!!")
+
+function mostrarResultado() {
+    let pontos = Number (localStorage.getItem("pontos")) || 0;
+    let lugar + document.getElementById("resultadoFinal);
+
+    if (lugar {
+      lugar.textContent=pontos + " de 6";
+    }
+}
+
+mostrarResultado();
